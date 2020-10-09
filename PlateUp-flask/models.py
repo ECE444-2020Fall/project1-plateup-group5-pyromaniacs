607dc69a05091195e218998904ae2ccc55951f04
Kevin Zhang <34317307+UTkzhang@users.noreply.github.com>
2020-10-09 12:04:26 -0400

    Initial flask setup (#20)
    * flask backend infrastructure initial setup commit
    
    * typo fix on initializer
    
    * test
    
    * test2
    
    * fixed email.ini and committed template
    
    * server up
    
    * email tested
    
    * email tested
    
    * docker for flask tested
    
    * Create README.md
    
    * updated gitignore
    
    * fixed ds store issues
    
    * fixed status code on mail
    
    * fixed status code on conflict
    
    * changed python to python3 Dockerfile
    
    * Update README.md
    
    * react folder rename
    
    * fixed imports ordering
    
    * Several formatting changes, along with some logic changes:
    1. UUIDs for the user are converted to strings
    2. Fixed bare except in emailservice
    3. GET for UserAPI returns results, not results.data, as it isn't a thing
    4. Changed id field for LoginAPI to string from int
    5. Removed unnecessary argument to logout_user
    6. Get user ID of user logging out before they logout
    7. user_loader passes the UID directly instead of converting it to int
    
    Co-authored-by: Eliano Anile <elianoanile@gmail.com>

---
+++ PlateUp-flask/models.py
@@ -0,0 +1,42 @@
+from flask_login import UserMixin
+from initializer import db
+from uuid import uuid1
+
+
+# -----------------------------------------------------------------------------
+# DB Models
+# -----------------------------------------------------------------------------
+# User table
+class User(db.Model, UserMixin):
+    __tablename__ = "user"
+
+    id = db.Column(db.String(40), primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(40))
+    password = db.Column(db.String)
+    settings_id = db.Column(db.String(40))
+    shopping_id = db.Column(db.String(40))
+    inventory_id = db.Column(db.String(40))
+
+    def __init__(self, name, email, password):
+        self.id = str(uuid1())
+        self.name = name
+        self.email = email
+        self.password = password
+        self.settings_id = str(uuid1())
+        self.shopping_id = str(uuid1())
+        self.inventory_id = str(uuid1())
+
+
+# shopping table
+class ShoppingList(db.Model):
+    __tablename__ = "shoppinglist"
+
+    id = db.Column(db.String(40), db.ForeignKey('user.shopping_id'), primary_key=True)
+    ingredient_name = db.Column(db.String(50))
+    quantity = db.Column(db.Integer)
+
+    def __init__(self, id, ingredient_name, quantity):
+        self.id = id
+        self.ingredient_name = ingredient_name
+        self.quantity = quantity
---
+++ README.md
@@ -0,0 +1,12 @@
+# project1-plateup-group5-pyromaniacs
+project1-webapp-group5-pyromaniacs created by GitHub Classroom
+
+# Instructions (Local)
+To build and start the system, first clone the repository <br>
+```git clone https://github.com/ECE444-2020Fall/project1-plateup-group5-pyromaniacs && cd project1-plateup-group5-pyromaniacs```<br><br>
+
+Backend:
+If you don't have docker, please install it first: https://docs.docker.com/get-started/ <br>
+To start the docker container, simply use docker-compose.<br>
+```cd PlateUp-flask && docker-compose up```<br><br>
+You can use ctrl-C to exit the container.
