2bcb254d3a6f75adc0a5e410da57899365b3ccf0
Eliano Anile <elianoanile@gmail.com>
2020-11-04 18:10:32 -0500

    UI of list complete

--- PlateUp-reactnative/components/List.js
+++ PlateUp-reactnative/components/List.js
@@ -1,44 +1,100 @@
 import { Block, Text } from 'galio-framework';
+import PropTypes from 'prop-types';
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
-import PropTypes from 'prop-types';
+import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
 
 import Icon from './Icon';
 import { argonTheme } from '../constants';
+import { toast } from '../constants/utils';
 
 class List extends React.Component {
+  handleAddItem = () => {
+    // Alert.prompt(
+    //   'Ingredient Name',
+    //   'Quantity',
+    //   [
+    //     {
+    //       text: 'Cancel',
+    //       onPress: () => console.log('Cancel Pressed'),
+    //       style: 'cancel'
+    //     },
+    //     {
+    //       text: 'OK',
+    //       onPress: console.log('OK Pressed')
+    //     }
+    //   ],
+    // );
+
+    const { onAddItem, items } = this.props;
+
+    const key = '';
+    const val = '';
+
+    if (items.some((item) => item.key === key)) {
+      toast(`Cannot add "${key}" as it already exists!`);
+      return;
+    }
+
+    onAddItem(key, val);
+  }
+
+  handleDeleteItem = (key) => {
+    const { onDeleteItem } = this.props;
+    onDeleteItem(key);
+  }
+
+  renderAddItem = () => (
+    <TouchableOpacity
+      style={styles.addItemContainer}
+      onPress={() => this.handleAddItem()}
+    >
+      <Icon
+        name="add"
+        family="MaterialIcons"
+        size={24}
+        color={argonTheme.COLORS.PRIMARY}
+        style={styles.addIcon}
+      />
+      <Block style={{ padding: 15 }}>
+        <Text style={styles.text} bold color={argonTheme.COLORS.PRIMARY}>
+          Add Item
+        </Text>
+      </Block>
+    </TouchableOpacity>
+  )
+
   renderItems() {
-    const { onDeleteItem, onAddItem, items } = this.props;
+    const { items } = this.props;
 
     return (
-      <ScrollView style={styles.listContainer}>
+      <ScrollView showsVerticalScrollIndicator={false}>
         {
-          items.map((item) => {
-            const { ingredient, quantity } = item;
-            const key = ingredient;
+        items.map((item) => {
+          const { ingredient, quantity } = item;
+          const key = ingredient;
 
-            return (
-              <Block key={key} flex row>
-                <Icon
-                  name="close"
-                  family="Ionicons"
-                  size={24}
-                  color={argonTheme.COLORS.TEXT_COLOR}
-                  style={styles.deleteIcon}
-                  onPress={() => onDeleteItem(key)}
-                />
-                <Block style={styles.itemContainer}>
-                  <Text style={styles.text}>
-                    {ingredient}
-                  </Text>
-                  <Text style={styles.text}>
-                    {quantity}
-                  </Text>
-                </Block>
+          return (
+            <Block key={key} row>
+              <Icon
+                name="close"
+                family="Ionicons"
+                size={24}
+                color={argonTheme.COLORS.TEXT_COLOR}
+                style={styles.deleteIcon}
+                onPress={() => this.handleDeleteItem(key)}
+              />
+              <Block style={styles.listItemContainer}>
+                <Text style={styles.text}>
+                  {ingredient}
+                </Text>
+                <Text style={styles.text}>
+                  {quantity}
+                </Text>
               </Block>
-            );
-          })
+            </Block>
+          );
+        })
         }
       </ScrollView>
     );
@@ -46,8 +102,9 @@ class List extends React.Component {
 
   render() {
     return (
-      <Block flex>
+      <Block style={styles.listContainer}>
         {this.renderItems()}
+        {this.renderAddItem()}
       </Block>
     );
   }
@@ -61,21 +118,26 @@ List.propTypes = {
 
 const styles = StyleSheet.create({
   listContainer: {
-    flex: 1,
     margin: 20,
+  },
+  listItemContainer: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    flex: 1,
+    borderBottomColor: argonTheme.COLORS.MUTED,
+    borderBottomWidth: StyleSheet.hairlineWidth * 0.9,
+    padding: 15,
+  },
+  addItemContainer: {
+    flexDirection: 'row',
   },
   deleteIcon: {
     paddingRight: 10,
     alignSelf: 'center',
   },
-  itemContainer: {
-    justifyContent: 'space-between',
-    flexDirection: 'row',
-    flex: 1,
-    borderBottomColor: argonTheme.COLORS.MUTED,
-    borderBottomWidth: StyleSheet.hairlineWidth,
-    paddingVertical: 15,
-    paddingHorizontal: 15,
+  addIcon: {
+    paddingRight: 10,
+    alignSelf: 'center',
   },
   text: {
     fontSize: 18,
