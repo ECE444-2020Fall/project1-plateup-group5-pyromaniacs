ad54f36453a9522f29a931abf48dea96790a550b
Eliano Anile <elianoanile@gmail.com>
2020-11-04 17:05:10 -0500

    List component doesn't do the adding/deleting itself. The caller must define those functions for it. As a result, it also doesn't maintain its own state, but instead receives items to display through its props

--- PlateUp-reactnative/screens/Inventory.js
+++ PlateUp-reactnative/screens/Inventory.js
@@ -5,10 +5,28 @@ import React from 'react';
 
 import { List } from '../components';
 
+const testItems = [
+  { ingredient: 'Milk', quantity: '2L' },
+  { ingredient: 'Ground Beef', quantity: '500g' },
+  { ingredient: 'Shrimp', quantity: '300g' }
+];
+
 class Inventory extends React.Component {
+  handleDeleteItem = (key) => {
+    console.log(`Item with key '${key}' deleted!`);
+  }
+
+  handleAddItem = (key, val) => {
+    console.log(`Item with (key,val) '(${key},${val})' deleted!`);
+  }
+
   render() {
     return (
-      <List />
+      <List
+        items={testItems}
+        onAddItem={this.handleAddItem}
+        onDeleteItem={this.handleDeleteItem}
+      />
     );
   }
 }
