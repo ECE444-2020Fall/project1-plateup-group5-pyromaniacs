0b0b04b33a1a0745ebf91e9fa0e0292a7e7ce682
UTkzhang <kevinzhanghk2012@gmail.com>
2020-11-08 19:00:10 -0500

    addressed comments

--- PlateUp-flask/run.py
+++ PlateUp-flask/run.py
@@ -503,7 +503,7 @@ class RecipeInventoryCheckerAPI(Resource):
         for entry in required:
             if entry in inventory:
                 if required[entry]['unit'] != inventory[entry]['unit']:
-                    return Response("Bad unit match while checking ingredient requirements for recipe.", status=200)
+                    return Response("Bad unit match while checking ingredient requirements for recipe.", status=400)
                 if inventory[entry]['quantity'] - required[entry]['quantity'] >= 0:
                     inventory[entry]['quantity'] -= required[entry]['quantity'] 
                 else:
@@ -656,7 +656,7 @@ class ShoppingFlashToInventoryAPI(Resource):
             else:
                 inventory_entry = Inventory.query.get((user_id, entry.ingredient_name))
                 if entry.unit != inventory_entry.unit:
-                    return Response("Bad unit match while flashing to inventory.")
+                    return Response("Bad unit match while flashing to inventory.", status=400)
                 inventory_entry.quantity = inventory_entry.quantity + entry.quantity
 
         shopping_res = ShoppingList.query.filter_by(user_id=user_id).delete()
@@ -828,7 +828,7 @@ if __name__ == '__main__':
     db.create_all()
     scheduler.start()
     updateRecipesToDB()
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=False)
 
     # Terminate background tasks
     scheduler.shutdown()
